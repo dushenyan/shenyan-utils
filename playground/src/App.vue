2a482b73807c184da11faad4f589a7638aedff41
dbudaiya <2645472474@qq.com>
2022-10-29 16:28:43 +0800

    feat: 客户端运行模板

---
+++ playground/src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { getBrowserType } from 'shenyan-utils';
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+const browser = ref(getBrowserType())
+</script>
+
+<template>
+  <div>
+    <h2>{{ browser }}</h2>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo">
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
